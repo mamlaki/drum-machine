338db0785123eedab213be38dff46004fc2dbce2
mamlaki <melekredwan@icloud.com>
2023-11-06 01:52:15 -0700

    App.js: Display text will now revert to the last used drum key after the user changes the volume.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import DrumPad from './components/DrumPad'
 
 export default function App() {
   const [displayText, setDisplayText] = useState('')
   const [volume, setVolume] = useState(0.5)
+  const displayTimeoutRef = useRef(null)
+  const previousDisplayTextRef = useRef('')
 
   const handleVolumeChange = (event) => {
     const newVolume = Number(event.target.value)
     setVolume(newVolume)
-    setDisplayText(`Volume: ${Math.round(newVolume * 100)}`)
+    clearTimeout(displayTimeoutRef.current)
+   
+    if (displayText.includes('Volume:')) {
+      setDisplayText(`Volume: ${(newVolume * 100).toFixed(0)}`)
+    } else {
+      previousDisplayTextRef.current = displayText
+      setDisplayText(`Volume: ${(newVolume * 100).toFixed(0)}`)
+    }
+
+    displayTimeoutRef.current = setTimeout(() => {
+      setDisplayText(previousDisplayTextRef.current)
+    }, 1000)
+  }
+
+  const handleDisplayText = (text) => {
+    clearTimeout(displayTimeoutRef.current)
+    previousDisplayTextRef.current = text
+    setDisplayText(text)
+
+    displayTimeoutRef.current = setTimeout(() => {
+      setDisplayText(previousDisplayTextRef.current)
+    }, 1000)
+    
   }
 
   return (
@@ -17,11 +41,11 @@ export default function App() {
       <div id='drum-machine' className='border border-5 border-warning rounded p-4 d-flex justify-content-center align-items-center bg-secondary '>
         <div className='row'>
           <div className='col-md-6'>
-            <DrumPad setDisplayText={setDisplayText} volume={volume} />
+            <DrumPad setDisplayText={handleDisplayText} volume={volume} />
           </div>
           <div className='col-md-6 d-flex align-items-center justify-content-center flex-column '>
             <div id='display' className='bg-dark p-4 rounded text-center w-50 fs-5'>{displayText}</div>
-            <input type='range' className='form-range mt-3' min='0' max='1' step='0.01' id='volumeControl' onChange={handleVolumeChange} />
+            <input type='range' className='form-range mt-3' min='0' max='1' step='0.01' value={volume} id='volumeControl' onChange={handleVolumeChange} />
           </div>
         </div>
       </div>

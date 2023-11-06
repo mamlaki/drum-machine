6de5a1e8c22d2572e561f9b23500deee602ff7af
mamlaki <melekredwan@icloud.com>
2023-11-05 20:59:49 -0700

    General: Adjusted the colour palette and positioned the machine display. - Now using custom drop shadow instead of Boostrap for drumkeys. - Now using custom width instead of Bootstrap for width.

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ export default function App() {
   const [displayText, setDisplayText] = useState('')
 
   return (
-    <div className='vh-100 d-flex align-items-center justify-content-center bg-secondary text-white fw-bold'>
-      <div id='drum-machine' className='border border-5 border-warning rounded p-4 d-flex justify-content-center align-items-center bg-dark'>
+    <div className='vh-100 d-flex align-items-center justify-content-center bg-dark text-white fw-bold'>
+      <div id='drum-machine' className='border border-5 border-warning rounded p-4 d-flex justify-content-center align-items-center bg-secondary '>
         <div className='row'>
           <div className='col-md-6'>
             <DrumPad setDisplayText={setDisplayText} />
           </div>
-          <div className='col-md-6'>
-            <div id='display' className='bg-dark p-4 rounded'>{displayText}</div>
+          <div className='col-md-6 d-flex align-items-center justify-content-center'>
+            <div id='display' className='bg-dark p-4 rounded text-center w-50 fs-5'>{displayText}</div>
           </div>
         </div>
       </div>

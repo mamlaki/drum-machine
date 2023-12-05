bac0d135a5cf98c677975b482263de4df5c6fd59
mamlaki <melekredwan@icloud.com>
2023-12-05 00:57:40 -0700

    App.js/App.css: Adjusted power button styling and added power indication lights.

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ export default function App() {
   const previousDisplayTextRef = useRef('')
   const knobRef = useRef(null)
 
+  const buttonAudio = new Audio('/audio/hollow-button-click.mp3')
+  buttonAudio.volume = 0.1
+
   useEffect(() => {
     const knob = knobRef.current
     if (!knob || !isPoweredOn) {
@@ -80,9 +83,28 @@ export default function App() {
       <div id='drum-machine' className='border border-5 border-warning rounded p-4 d-flex justify-content-center align-items-center bg-secondary '>
         <div className='row'>
           <div className='col-md-6 d-flex align-items-center justify-content-center flex-column gap-3'>
-            <button onClick={() => setIsPoweredOn(prev => !prev)} className='power-button'>
-              Power
-            </button>
+            <div className='d-flex gap-3'>
+              <div className='d-flex flex-column text-center'>
+                <p className='power-btn-label text-warning mb-1'>POWER</p>
+                <div className='power-btn-container d-flex p-2 justify-content-center'>
+                  <button onClick={() => {
+                    setIsPoweredOn(prev => !prev)
+                    buttonAudio.play()
+                  }} className='power-button bg-dark shadow'>
+                  </button>
+                </div>
+              </div>
+              <div className='power-light-container d-flex flex-column'>
+                <div className='d-flex align-items-baseline gap-2'>
+                  <div className={`on-off-lights ${isPoweredOn ? 'bg-success' : 'bg-dark'}`}></div>
+                  <p className='text-warning'>ON</p>
+                </div>
+                <div className='d-flex align-items-baseline gap-2'>
+                  <div className={`on-off-lights ${isPoweredOn ? 'bg-dark' : 'bg-danger'}`}></div>
+                  <p className='text-warning'>OFF</p>
+                </div>
+              </div>
+            </div>
             <div id='display' className='bg-dark p-4 rounded text-center w-50 fs-5'>{displayText}</div>
             <div 
               ref={knobRef} 

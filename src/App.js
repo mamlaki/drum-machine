3d2d5c86cd3ebfb3d7e2db876ee32651672fa0c5
Melek Redwan <melekredwan@icloud.com>
2023-12-09 15:59:15 -0700

    App.js: Refactored the volume knob and redesigned it. - Moved volume knob related code into a separate component. - Visual adjustments were made to the volume knob. - Adjusted the turning radius. - Still some issues to fix.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState, useRef, useEffect } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import DrumPad from './components/DrumPad'
+import VolumeKnob from './components/VolumeKnob'
 
 export default function App() {
   const [displayText, setDisplayText] = useState('')
@@ -12,62 +13,6 @@ export default function App() {
 
   const buttonAudio = new Audio('/audio/hollow-button-click.mp3')
   buttonAudio.volume = 0.1
-
-  useEffect(() => {
-    const knob = knobRef.current
-    if (!knob || !isPoweredOn) {
-      return
-    }
-
-    const handleMouseMove = (event) => {
-      const knobRect = knobRef.current.getBoundingClientRect()
-
-      const centerX = knobRect.left + knobRect.width / 2
-      const centerY = knobRect.top + knobRect.height / 2
-
-      const dx = event.clientX - centerX
-      const dy = event.clientY - centerY
-      
-      let angle = Math.atan2(dy, dx) * (180 / Math.PI)
-      angle = (angle + 360) % 360
-  
-      let normalizedAngle = (angle - 45 + 360) % 360
-      normalizedAngle = Math.max(0, Math.min(normalizedAngle, 270))
-  
-      let newVolume = normalizedAngle / 270
-      console.log('Setting new volume: ', newVolume)
-
-      setVolume(newVolume)
-      updateDisplayVolume(newVolume)
-    }
-
-    const handleMouseUp = () => {
-      document.removeEventListener('mousemove', handleMouseMove)
-      document.removeEventListener('mouseup', handleMouseUp)
-    }
-
-    const handleMouseDown = () => {
-      document.addEventListener('mousemove', handleMouseMove)
-      document.addEventListener('mouseup', handleMouseUp, { once: true})
-    }
-
-    knob.addEventListener('mousedown', handleMouseDown)
-
-    return () => {
-      knob.removeEventListener('mousedown', handleMouseDown)
-    }
-  }, [isPoweredOn])
-
-
-  const updateDisplayVolume = (newVolume) => {
-    clearTimeout(displayTimeoutRef.current)
-    setDisplayText(`Volume: ${(newVolume * 100).toFixed(0)}`)
-    displayTimeoutRef.current = setTimeout(() => {
-      if (isPoweredOn) {
-        setDisplayText(previousDisplayTextRef.current)
-      }
-    }, 1000)
-  }
 
   const handleDisplayText = (text) => {
     clearTimeout(displayTimeoutRef.current)
@@ -106,15 +51,19 @@ export default function App() {
               </div>
             </div>
             <div id='display' className='bg-dark p-4 rounded text-center w-50 fs-5'>{displayText}</div>
-            <div 
+            <VolumeKnob 
+              volume={volume}
+              setVolume={setVolume}
+              isPoweredOn={isPoweredOn}
+            />
+            {/* <div 
               ref={knobRef} 
               id='volume-control' 
               className='volume-knob' 
               style={{ 
                 transform: `rotate(${volume * 270 - 45}deg)`, 
-                boxShadow: `0px ${4 + volume * 4}px ${8 + volume * 4}px rgba(0, 0, 0, ${0.3 + volume * 0.2})`
-              }}>  
-            </div>
+              }}>
+            </div> */}
           </div>
           <div className='col-md-6'>
             <DrumPad setDisplayText={handleDisplayText} volume={volume} isPoweredOn={isPoweredOn} />
---
+++ src/components/VolumeKnob.js
@@ -0,0 +1,159 @@
+import { useRef, useEffect } from 'react'
+
+export default function VolumeKnob({ volume, setVolume, isPoweredOn }) {
+  const knobRef = useRef(null)
+  const numberOfNotches = 11
+  const knobDiameter = 100
+
+  useEffect(() => {
+    console.log('useEffect running, isPoweredOn:', isPoweredOn);
+
+    const knob = knobRef.current
+    if (!knob || !isPoweredOn) {
+      return
+    }
+
+    const handleMouseMove = (event) => {
+      const knobRect = knobRef.current.getBoundingClientRect()
+
+      const centerX = knobRect.left + knobRect.width / 2
+      const centerY = knobRect.top + knobRect.height / 2
+
+      const dx = event.clientX - centerX
+      const dy = event.clientY - centerY
+      
+      let angle = Math.atan2(dy, dx) * (180 / Math.PI)
+      angle = (angle + 360) % 360
+  
+      let normalizedAngle = angle - 45
+      normalizedAngle = Math.max(0, Math.min(normalizedAngle, 270))
+  
+      let newVolume = normalizedAngle / 270
+      console.log('Setting new volume: ', newVolume)
+
+      setVolume(newVolume)
+      console.log(`Angle: ${angle}, Normalized Angle: ${normalizedAngle}, New Volume: ${newVolume}`);
+      // updateDisplayVolume(newVolume)
+    }
+
+    const handleMouseUp = () => {
+      document.removeEventListener('mousemove', handleMouseMove)
+      document.removeEventListener('mouseup', handleMouseUp)
+    }
+
+    const handleMouseDown = () => {
+      console.log('MouseDown event attached');
+      document.addEventListener('mousemove', handleMouseMove)
+      document.addEventListener('mouseup', handleMouseUp, { once: true })
+      console.log('test')
+    }
+
+    knob.addEventListener('mousedown', handleMouseDown)
+
+    return () => {
+      knob.removeEventListener('mousedown', handleMouseDown)
+    }
+  }, [isPoweredOn])
+
+
+  const calculatePosition = (index, numberOfNotches, diameter, offset) => {
+    const angle = (index / (numberOfNotches - 1)) * 300 - 150
+    const angleInRadians = (angle * Math.PI) / 180
+
+    const radiusForNotches = diameter / 2
+    const radiusForNumbers = (diameter / 2) + offset
+
+    const xForNotches = (diameter / 2) + (radiusForNotches * Math.cos(angleInRadians))
+    const yForNotches = (diameter / 2) + (radiusForNotches * Math.sin(angleInRadians))
+    const xForNumbers = (diameter / 2) + (radiusForNumbers * Math.cos(angleInRadians))
+    const yForNumbers = (diameter / 2) + (radiusForNumbers * Math.sin(angleInRadians))
+
+    return {
+      xForNotches,
+      yForNotches,
+      xForNumbers,
+      yForNumbers,
+      angle
+    }
+  }
+
+  const notchesAndNumbers = []
+  const offset = 10
+  for (let i = 0; i < numberOfNotches; i++) {
+    const { xForNotches, yForNotches, xForNumbers, yForNumbers } = calculatePosition(i, numberOfNotches, knobDiameter, offset)
+
+    notchesAndNumbers.push(
+      <div 
+        key={`notch-${i}`}
+        className='knob-notch'
+        style={{
+          left: `${xForNotches}px`,
+          top: `${yForNotches}px`,
+          transform: 'translate(-50%, -50%)'
+        }}
+      />
+    )
+
+    notchesAndNumbers.push(
+      <div
+        key={`number-${i}`}
+        className='knob-number'
+        style={{
+          left: `${xForNumbers - 10}px`,
+          top: `${yForNumbers + 10}px`,
+          transform: `rotate(90deg)`,
+          transformOrigin: 'center center'
+        }}
+      >
+        {i}
+      </div>
+    )
+  }
+
+  const createRidges = (numberOfRidges) => {
+    let clipPath = 'polygon('
+
+    for (let i = 0; i <= numberOfRidges; i++) {
+      const angle = (2 * Math.PI * i) / numberOfRidges
+      const x = 50 + 48 * Math.cos(angle)
+      const y = 50 + 48 * Math.sin(angle)
+      clipPath += `${x}% ${y}%, `
+
+      const ridgeAngle = angle + Math.PI / numberOfRidges
+      const ridgeX = 50 + 50 * Math.cos(ridgeAngle)
+      const ridgeY = 50 + 50 * Math.sin(ridgeAngle)
+      clipPath += `${ridgeX}% ${ridgeY}%${i < numberOfRidges ? ', ' : ''}`
+    }
+
+    clipPath += ')'
+    return clipPath
+  }
+
+  const clipPathStyling = {
+    clipPath: createRidges(41)
+  }
+
+  return (
+    <div
+      id='volume-control-container'
+      className='volume-control-container'
+      style={{
+        width: `${knobDiameter}px`,
+        height: `${knobDiameter}px`
+      }}
+    >
+      <div className='notches-and-numbers'>
+        {notchesAndNumbers}
+      </div>
+      <div 
+        ref={knobRef}
+        className='volume-knob'
+        style={{
+          transform: `rotate(${volume * 300 - 155}deg) scale(1.35)`
+        }}
+      >
+        <div className='inner-knob' style={clipPathStyling}></div>
+      </div>
+    </div>
+  )
+}
